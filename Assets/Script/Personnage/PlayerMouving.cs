383f9fee3e6375326bdeddb1f70a1850c390b67e
BenjaminG <georges.benjamin00@gmail.com>
2022-09-13 09:41:55 +0200

    first try

---
+++ Assets/Script/Personnage/PlayerMouving.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouving : MonoBehaviour
+{
+    public float Speed = 0.01f;
+    public Vector2 turn;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z)){
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S)){
+            transform.Translate(Vector3.back * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Q)){
+            transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+        }
+        turn.x += Input.GetAxis("Mouse X");
+        turn.y += Input.GetAxis("Mouse Y");
+        transform.localRotation = Quaternion.Euler(0 , turn.x, 0);
+    }
+}
